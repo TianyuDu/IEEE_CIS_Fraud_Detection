12dae8570f7c1e4e032b4d76f22aa2389798be8d
MasterDoo <masterdu9951@icloud.com>
2019-08-24 11:18:16 +0800

    add categorical feature lists

---
+++ utils/features.py
@@ -0,0 +1,30 @@
+"""
+Aug. 21, 2019
+Use this script to store methods that:
+i. Manipulate coulumns of dataset, like creating new features.
+"""
+import pandas as pd
+
+
+# Categorical columns in transaction dataset.
+CATEGORICAL_TRANS = sum(
+    [
+        ["card" + str(i) for i in range(1, 7)],
+        ["addr1", "addr2"],
+        ["P_emaildomain", "R_emaildomain"],
+        ["M" + str(i) for i in range(1, 10)]
+    ],
+    ["ProductCD"]
+)
+
+CATEGORICAL_ID = ["DeviceType", "DeviceInfo"] + [
+    "id_" + str(i) for i in range(12, 39)
+]
+
+
+def clean_dat(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Cleans the original dataset and formulates it so that
+    it is compatible with most existing ML models.
+    """
+
