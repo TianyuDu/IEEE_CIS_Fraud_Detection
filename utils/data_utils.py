8284adea84350dba61c0057eb50272fe1e927813
MasterDoo <masterdu9951@icloud.com>
2019-08-24 10:28:16 +0800

    add sample_dataset

---
+++ utils/data_utils.py
@@ -0,0 +1,58 @@
+"""
+Aug. 21, 2019
+Use this script to store methods that:
+i. Manipulate datasets via direct interactions with .csv files on disk.
+ii. Spliting and concanating datasets.
+
+NOTE:
+Methods in this script do NOT create new columns (features).
+"""
+from typing import Union
+import numpy as np
+import pandas as pd
+
+
+def load_dataset(path: str) -> pd.DataFrame:
+    """
+    Load the dataset from *_forcus.csv, rename the complete dataset
+    to *_focus.csv to load it.
+    """
+    df_train = pd.read_csv(path + "/train_transaction_focus.csv")
+    df_test = pd.read_csv(path + "/test_transaction_focus.csv")
+    X_train, y_train = split_data(df_train)
+
+
+def split_data():
+    raise NotImplementedError
+
+
+def sample_dataset(
+    path: str,
+    data: str,
+    n: Union[int, float] = 0.05,
+    random_state: int = 42
+) -> None:
+    """
+    Creates a subsample of the entire training set, to improve performance of model prototyping.
+    A [train, test]_[tr, id]_focus.csv file
+    containing the sub-sampled dataset will be stored in path provided.
+
+    Args:
+        path: data directory.
+        n: size of subsample if n >= 1, percentage if n < 1.
+        data: which dataset to use, either 'train' or 'test'.
+    """
+    p = "{}/{}_{}.csv"
+    df_trans = pd.read_csv(p.format(path, data, "transaction"))
+    # We leave the secondary dataset out for now.
+    # df_id = pd.read_csv(p.format(path, data, "identity"))
+    if n < 1:
+        n = int(n * len(df_trans))
+    print("{}/{} {} instances will be sampled.".format(n, len(df_trans), data))
+    selected_id = df_trans.sample(n, random_state=42).TransactionID
+    mask = df_trans.TransactionID.isin(selected_id)
+    df_trans_sub = df_trans[mask].reset_index(drop=True)
+    df_trans_sub.to_csv(path + "/{}_{}_focus.csv".format(data, "transaction"), index=False)
+
+if __name__ == "__main__":
+    pass
