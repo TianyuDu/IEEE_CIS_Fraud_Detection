321fc6af8b30926a537f2c1edc46d93caac7091a
MasterDoo <masterdu9951@icloud.com>
2019-08-27 22:45:34 +0800

    implement generate_feature_columns

--- utils/tf_utils.py
+++ utils/tf_utils.py
@@ -2,3 +2,42 @@
 Utilities for tensorflow.
 """
 import tensorflow as tf
+
+from utils import features
+
+
+def generate_feature_columns(
+    X: pd.DataFrame,
+    y: Union[pd.DataFrame, None],
+    ID: str = "TransactionID"
+) -> Tuple[List[tf.feature_column]]:
+    """
+    Generates the correspond feature columns from feature and target datasets.
+    Args:
+        X: feature dataset for the problem.
+        y: target dataset for supervised learning problem.
+    Returns:
+        (X_fea_col, y_fea_col): feature columns for features and target.
+    """
+    # ==== y ====
+    y_fea_col = tf.feature_column.categorical_column_with_identity(key="isFraud", num_buckets=2)
+    y_fea_col = tf.feature_column.indicator_column(y_fea_col)
+    # ==== X ====
+    X_fea_col = []
+    for col in X.columns:
+        if col == ID:
+            continue
+        if col in features.CATEGORICAL_TRANS or col in features.CATEGORICAL_ID:
+            # Categorical features:
+            num_categories = len(set(X[col]))
+            identity_feature_column = tf.feature_column.categorical_column_with_identity(
+                key=col,
+                num_buckets=num_categories
+            )
+            identity_feature_column = tf.feature_column.indicator_column(identity_feature_column)
+            X_fea_col.append(identity_feature_column)
+        else:
+            # Numerical features:
+            numerical_feature_column = tf.feature_column.numeric_column(key=col)
+            X_fea_col.append(numerical_feature_column)
+    return X_fea_col, y_fea_col
