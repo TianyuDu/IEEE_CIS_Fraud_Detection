a32febb419241e8fdc7301e936b76da27cffdf99
MasterDoo <masterdu9951@icloud.com>
2019-08-29 00:48:49 +0800

    update api

--- data_proc_api.py
+++ data_proc_api.py
@@ -10,8 +10,17 @@ from feature import feature_engineering
 
 if __name__ == "__main__":
     # Load the dataset with nan filled.
-    (X_train, y_train), (X_test, y_test) = data_utils.load_dataset(path="./data")
+    X_train, y_train, X_test = data_utils.load_dataset(path="./data")
+    s = "Datasets received: X_train.shape={}; y_train.shape={}, X_test.shape={}"
+    print(s.format(X_train.shape, y_train.shape, X_test.shape))
+
     assert np.all(X_train.index == y_train.index)
     train_index = X_train.index
     test_index = X_test.index
-    X_all = np.concatenate([X_train, X_test], axis=0)
+
+    X_merged = pd.concat([X_train, X_test], axis=0)
+    X_merged = feature_engineering.main_fe(X_merged)
+
+    X_train = X_merged.loc[train_index]
+    X_test = X_merged.loc[test_index]
+    assert np.all(X_train.index == y_train.index)
