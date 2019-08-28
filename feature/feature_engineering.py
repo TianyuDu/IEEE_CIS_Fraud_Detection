1b03344bdd00b0f58429053333f26576d251b93a
MasterDoo <masterdu9951@icloud.com>
2019-08-29 00:49:00 +0800

    misc

--- feature/feature_engineering.py
+++ feature/feature_engineering.py
@@ -8,10 +8,13 @@ from utils import data_utils
 from utils import feature_utils
 
 
-def modify_feature(df: pd.DataFrame) -> pd.DataFrame:
+def main_fe(df: pd.DataFrame) -> pd.DataFrame:
     """
     All in one method for feature engineering.
     """
     df = df.copy()
-    df = pca_and_cluster(df, columns="V", n_components=35, n_clusters=6)
+    # Modify numerical features.
+    df = feature_utils.pca_and_cluster(df, columns="V", n_components=35, n_clusters=6)
+    df = feature_utils.pca_and_cluster(df, columns="C", n_components=3, n_clusters=4)
+    df = feature_utils.pca_and_cluster(df, columns="D", n_components=3, n_clusters=8)
     return df
