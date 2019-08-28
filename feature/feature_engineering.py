856f178b73230f1a2a3777af0f8e04889aff4a19
MasterDoo <masterdu9951@icloud.com>
2019-08-28 17:11:10 +0800

    add modify_feature

--- feature/feature_engineering.py
+++ feature/feature_engineering.py
@@ -6,3 +6,12 @@ import pandas as pd
 
 from utils import data_utils
 from utils import feature_utils
+
+
+def modify_feature(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    All in one method for feature engineering.
+    """
+    df = df.copy()
+    df = pca_and_cluster(df, columns="V", n_components=35, n_clusters=6)
+    return df
